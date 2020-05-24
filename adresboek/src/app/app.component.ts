504dcefaef4ef24eb440a6b0dbd962fc95cb0be9
Roland Verschueren <Medow@192.168.2.117>
2020-05-24 14:00:09 +0200

    adresboek route

--- adresboek/src/app/app.component.ts
+++ adresboek/src/app/app.component.ts
@@ -2,19 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AddressBookService } from './services/address-book/addresses.service';
 import { Address } from './services/address-book/addresses.types';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'adresboek';
-  adressen$: Observable<Address[]>;
 
-  constructor(private adressenService: AddressBookService) { }
-
-  ngOnInit() {
-    this.adressen$ = this.adressenService.get();
+  constructor(router: Router) {
+    router.navigateByUrl('/adresboek');
   }
 }
